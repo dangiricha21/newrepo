066bf760becfcf5434472b5f22ee99b7130bb4e5
dangiricha21 <dangi.richa21@gmail.com>
2026-04-24 04:02:58 +0530

    set production API URL

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,8 +1,20 @@
+// import axios from 'axios';
+
+// const api=axios.create({
+//     baseURL:import.meta.env.VITE_BASEURL || 'http://localhost:3000',
+  
+//     withCredentials:true
+// })
+// export default api
+
+
+
+
 import axios from 'axios';
 
-const api=axios.create({
-    baseURL:import.meta.env.VITE_BASEURL || 'http://localhost:3000',
-  
-    withCredentials:true
-})
-export default api
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASEURL,
+    withCredentials: true
+});
+
+export default api;
