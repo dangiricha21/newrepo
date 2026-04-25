ec49168efb91222bab7897d4ed473373940feb72
dangiricha21 <dangi.richa21@gmail.com>
2026-04-25 23:35:16 +0530

    fix auth baseURL 405

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,10 +1,26 @@
-import { createAuthClient } from "better-auth/react"
+// import { createAuthClient } from "better-auth/react"
+
+// export const authClient = createAuthClient({
+    
+//     baseURL: import.meta.env.VITE_BASEURL,
+//     fetchOptions :{credentials:"include" },
+
+// })
+
+// export const { signIn, signUp, useSession } = authClient
+
+
+
+
+
+
+
+
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    
-    baseURL: import.meta.env.VITE_BASEURL,
-    fetchOptions :{credentials:"include" },
+  baseURL: `${import.meta.env.VITE_BASEURL}/api/auth`,
+  fetchOptions: { credentials: "include" },
+});
 
-})
-
-export const { signIn, signUp, useSession } = authClient
+export const { signIn, signUp, useSession } = authClient;
