2d326ebd8e539c8b320350c64aef8bd6a653c9b9
dangiricha21 <dangi.richa21@gmail.com>
2026-04-26 01:58:56 +0530

    force update prisma adapter

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,16 +1,28 @@
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from '@prisma/client';
+
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma
+
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const prisma = new PrismaClient({
+  adapter,
+});
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export default prisma
-
-
+export default prisma;
 
 
 
